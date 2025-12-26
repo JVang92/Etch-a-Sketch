72fc4a440cd9caab48655bd4b7c3946fc1f38160
ItVang <bigbossluong@gmail.com>
2025-12-26 13:16:46 -0600

    addded random color option

--- script.js
+++ script.js
@@ -4,27 +4,41 @@ const DARKNESS_SUBTRACT = 10;
 const gridAmountDisplay = document.getElementById("current_amount");
 const button = document.getElementById("grid_choice");
 const currentAmount = document.getElementById("grid_amount");
+const colorCheckBox = document.getElementById("random_color");
 const container = document.querySelector("#container");
 
 
 
 
-button.addEventListener("click", () => changeGrid());
 
+
+
+button.addEventListener("click", () => changeGrid());
+colorCheckBox.addEventListener("click", () => console.log(isChecked()));
+
+initializeGrid(); //create and display the default grid
+
+
+
+function initializeGrid(){
 for(let i = 0; i < GRID_SIZE * GRID_SIZE; i++){
     const square = document.createElement("div"); // used to create each square instead of pasting in HTML
     square.classList.add("square");
 
 
     square.addEventListener("pointerover", function(e) { // when mouse goes over a square, it is filled in
-        square.classList.add("hovered");
-        console.log(square.classList);
+        if(isChecked()){
+        square.style.backgroundColor = getRandomColor();
+        }
+        else{square.classList.add("hovered");}
+        
     })
 
     
     
     container.appendChild(square); // add squares in container div
 
+}
 }
 
 function changeGrid(){
@@ -41,10 +55,15 @@ function changeGrid(){
     for(let i = 0; i < num * num; i++){
     const square = document.createElement("div"); // used to create each square instead of pasting in HTML
     square.classList.add("square");
-    //square.textContent = i; debug purposes
+
 
     square.addEventListener("pointerover", function(e) { // when mouse goes over a square, it is filled in
-        square.classList.add("hovered");
+        if(isChecked()){
+            square.style.backgroundColor = getRandomColor();
+        }
+        else{square.classList.add("hovered");}
+        
+
     })
     container.appendChild(square); // add squares in container div
 }
@@ -70,4 +89,17 @@ function promptAmount(){
 
     return num;
 
+}
+
+function getRandomColor() { // return random hex color
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function isChecked(){
+    return colorCheckBox.checked;
 }
